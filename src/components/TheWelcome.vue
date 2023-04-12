d6e7ac72bd0e3ae53f338495d3b850b353894c37
gaotx <gaotx@hsit.com.cn>
2023-04-12 17:46:57 +0800

    feat 引入Element Plus

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,6 +8,14 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
+   <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+// 引入Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
